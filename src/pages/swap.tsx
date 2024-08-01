1eaf73879084d1054da53af1f6022cf912da97b7
masayuki <b.maca217@gmail.com>
2024-08-01 10:48:42 +0900

    Add meta tag & structureData

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -8,10 +8,19 @@ import Swap from '@/components/Swap'
 const SwapPage: NextPage = () => {
     const { chainId, currentAccount }: any = useContext(ChainContext)
 
+    const structuredData = {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "MooSwap",
+        "url": "https://mooswap-finance.vercel.app/"
+    };
+
     return (
         <>
             <Head>
                 <title>MooSwap</title>
+                <meta name="description" content="MooSwap is a decentralized exchange (DEX) available on the Polygon (PoS) for traders and liquidity providers." />
+                <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} />
                 <meta name="google-site-verification" content="MeeG3wA-7GLXGU8yjKeP8SrdVF1_97lRaok4pGgys4Q" />
             </Head>
             <Header page='swap'/>
